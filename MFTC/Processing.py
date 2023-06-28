257b715b77f8c48e0f90a318b676a15406a75541
mathematichael <132102264+mathematichael@users.noreply.github.com>
2023-06-28 21:18:37 +0200

    Update Processing.py

--- MFTC/Processing.py
+++ MFTC/Processing.py
@@ -5,16 +5,16 @@ df = pd.read_csv('combined_dataset.csv')
 
 #ONE HOT ENCODING
 #we combine the class columns from the third onwards into a single class column
-df["OneHotLabels"] = df.iloc[:, 2:].astype(str).apply(' '.join, axis=1)
+df["classes"] = df.iloc[:, 2:].astype(str).apply(' '.join, axis=1)
 
 #have to assign these unique labels to an integer system
-df["OneHotInts"] = df['OneHotLabels'].astype('category').cat.codes
+df["classLabels"] = df['classes'].astype('category').cat.codes
 
 #train test 70:30 split
 train, test = train_test_split(df, test_size=0.3, random_state=42)
 
 #convert to json using pandas
-train[['OneHotInts', 'text']].rename(columns={'OneHotInts': 'label', 'text': 'source'}) \
+train[['classLabels', 'text']].rename(columns={'classLabels': 'label', 'text': 'source'}) \
     .to_json("train.jsonl", orient='records', lines=True)
 test[['classLabels', 'text']].rename(columns={'classLabels': 'label', 'text': 'source'}) \
     .to_json("test.jsonl", orient='records', lines=True)
@@ -23,7 +23,7 @@ test[['classLabels', 'text']].rename(columns={'classLabels': 'label', 'text': 's
 df["verbalizers"] = df.iloc[:, 2:13].apply(
     lambda row: ','.join([col for col, val in zip(df.columns[2:13], row) if val]), axis=1)
 
-df = df.sort_values(by="OneHotInts")
+df = df.sort_values(by="classLabels")
 
 #the following will print the verbalizers as a list, which you can then copy and paste into a txt document for the verablizers file.
 #automating this part isnt easy since you cant save a python list into a text file. easy fix!
